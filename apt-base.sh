b55b57f54370ab0dac098bf20a3baf34479c04a2
obaluae-matheus <obaluae.matheus@hotmail.com>
2025-12-26 17:20:22 -0300

    update apt-base.sh

--- apt-base.sh
+++ apt-base.sh
@@ -16,23 +16,33 @@ apt install -y \
     libfontconfig-dev \
     libxcb1-dev \
     libfreetype-dev \
-    libwayland-dev \
-    libwlroots-dev \
+    libwlroots-0.18-dev \
     libinput-dev \
     wayland-protocols \
-    libxwayland-dev \
-    libxkbcommon-dev
+    libwayland-dev \
+    libxkbcommon-dev \
+    xwayland \
     dbus \
     dbus-x11 \
     xdg-utils \
     xdg-user-dirs \
     xdg-desktop-portal \
     xdg-desktop-portal-wlr \
-    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gtk \
     kitty \
     chromium \
     wayfire \
     waybar \
     swaybg \
     imagemagick \
-    rofi
+    rofi \
+    fonts-font-awesome \
+    fonts-fork-awesome \
+    fonts-noto-color-emoji \
+    fonts-hack-ttf \
+    fonts-jetbrains-mono \
+    zip \
+    gedit
+    
+ # Cache
+ fc-cache -fv
